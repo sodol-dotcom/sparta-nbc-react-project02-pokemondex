124df15215b0bb3271a894e2033b0395e35b22af
sodol-dotcom <sojeong0521@gmail.com>
2024-08-22 00:41:02 +0900

    feat(PokemonCard): add component to display individual Pokemon with 'Add' button feat(PokemonCard): 개별 포켓몬 표시 및 '추가' 버튼 구현

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const PokemonCard = () => {
+function PokemonCard ({ pokemon, onAdd }) {
   return (
-    <div>PokemonCard</div>
-  )
+    <div>
+      PokemonCard
+      <img src={pokemon.image} alt={pokemon.name} />    {/* alt는 이미지가 나오지 않을때의 대체 텍스트 */}
+      <h3>{pokemon.name}</h3>
+      <button onClick={() => onAdd(pokemon)}>추가</button>
+    </div>
+  );
 }
 
 export default PokemonCard
