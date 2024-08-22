7f5f77ba5aef9ac905828666ebbea6eeaba8e169
sodol-dotcom <sojeong0521@gmail.com>
2024-08-22 13:30:07 +0900

    feat(Routing): set up BrowserRouter and routes for Home and Pokedex 홈 페이지와 도감 페이지를 위해 'BrowserRouter'와 'Router' 컴포넌트를 사용해 리액트 라우터 구성

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+// App.jsx -> 라우터 설정을 포함한 최상위 컴포넌트
+
 import React from 'react'
-import PokemonCard from './components/PokemonCard'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './components/Home'
+import Pokedex from './components/Pokedex'
 
 const App = () => {
   return (
-    <layout>
-      <PokemonCard />
-    </layout>
-  )
+    <Router> {/* Router=BrowserRouter의 별칭. BrowserRouter로 Router를 감싼다. */}
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link> </li>
+          <li><Link to="/pokedex">Pokedex</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pokedex" element={<Pokedex />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+// Home.jsx -> 홈 페이지를 위한 컴포넌트
+import React from 'react';
+
+function Home() {
+  return (
+    <div>Home</div>
+    <p>This is home page!</p>
+  );
+}
+
+export default Home;
