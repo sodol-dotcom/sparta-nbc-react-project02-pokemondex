37d9d8dda21571b09403293717ac797d20f028f7
sodol-dotcom <sojeong0521@gmail.com>
2024-08-22 19:42:39 +0900

    feat(PokemonDex): Implement Pokemon selection and management functionality feat(PokemonDex): 포켓몬 선택 및 관리 기능 구현
    - Added state management for selected Pokemon using useState in PokemonDex.jsx
    (Pokemon.jsx에서 useState를 사용하여 선택된 포켓몬을 관리하는 상태 추가)
    - Implemented addPokemon and removePokemon functions for handling Pokemon selection and removal
    (포켓몬 선택 및 제거를 위한 addPokemon 및 removalPokemon 함수를 구현)
    - Created mock data file (mock.js) and imported MOCK_DATA into PokemonDex.jsx
    (mock.js 파일을 생성하고 MOCK_DATA를 PokemonDex.jsx로 import)
    - Passed MOCK_DATA to PokemonList component for rendering Pokemon list
    (MOCK_DATA를 PokemonList 컴포넌트에 전달하여 포켓몬 목록을 렌더링하도록 함)

--- src/components/PokemonDex.jsx
+++ src/components/PokemonDex.jsx
@@ -1,14 +1,40 @@
 // PokemonDex.jsx -> 도감 페이지를 위한 컴포넌트
 
-import React from 'react';
+import React, { useState } from 'react';
+import MOCK_DATA from '../mock';
+import Dashboard from './Dashboard';
 
 
 function PokemonDex() {
+
+  // useState를 사용하여 선택된 포켓몬 관리하는 상태 생성 -> 빈 배열?[], 빈 객체?{}
+  const [selectedPokemon, setselectedPokemon] = useState([]);
+
+  // 포켓몬 데이터 배열 (id, name, image 속성 포함)
+  const pokemonList = [
+    
+  ];
+
+  // 포켓몬을 선택하는 로직
+  const addPokemon = (pokemon) => {
+    if (selectedPokemon.length >= 6) {
+      alert('최대 6개의 포켓몬만 선택할 수 있습니다.');
+      return;
+    }
+    setselectedPokemon([...selectedPokemon, pokemon])
+  };
+
+  // 포켓몬을 제거하는 로직(필터)
+  const removePokemon = (pokemon) => {
+    setselectedPokemon(selectedPokemon.filter((p) => p.id !== pokemon.id));
+  };
+
   return (
-    <>
-        <h1>PokemonDex Page</h1>
-        <p>Explore the PokemonDex!</p>
-    </>
+    <div>
+      {/* prop */}
+      <Dashboard selectedPokemon={selectedPokemon} onRemove={removePokemon} />
+      <pokemonList pokemonList={MOCK_DATA} onAdd={addPokemon} />
+    </div>
   );
 }
 
